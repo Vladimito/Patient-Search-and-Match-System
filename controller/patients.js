c1843942c604a5704ef0dd180b90a584f21290b4
Vladimito <vldandrd@gmail.com>
2021-03-02 21:13:47 -0330

    added methods for patient controller

--- controller/patients.js
+++ controller/patients.js
@@ -15,3 +15,61 @@ const create = async (req, res) => {
 		res.send('client-side: The Patient data you entered is invalid')
 	}
 }
+
+
+const getOne = async(req,res) =>{
+	const patientToGet = req.params.id;
+	let db = req.db;
+	try{
+		let obj = await Patient.getPatientByID(db,patientToGet);
+		res.send(obj);
+	}catch(err){
+		res.send('There was an error while retrieving the patient. (err:'+err+')');
+		throw new Error(err);
+	}
+}
+
+const updateOne = async(req, res) =>{
+	const pUpdate = req.body
+	const patientID = req.params.id
+	let db = req.db;
+	try{
+		let msg = await Patient.update(db,patientID,pUpdate.bday,pUpdate.dday,pUpdate.ssn,pUpdate.passport,pUpdate.prefix,pUpdate.first,pUpdate.last,pUpdate.suffix,pUpdate.maiden,pUpdate.martial,pUpdate.race,ethnicity,pUpdate.gender,pUpdate.birthplace,pUpdate.address,pUpdate.city,pUpdate.county,pUpdate.zip,pUpdate.lat,pUpdate.low,pUpdate.healthExpenses,pUpdate.healthCoverage);
+		res.send(msg);
+	}catch(err){
+		res.send('There was an error while updating that patient\'s information. (err:'+err+')');
+		throw new Error(err);
+	}
+}
+
+const deleteOne = async (req, res) => {
+	const patientID = req.params.id;
+	let db = req.db;
+	try{
+		let msg = await Patient.delete(db, book_id);
+		res.send(msg);	
+	}catch(err){
+		res.send('There was an error while deleting your Book. (err:'+err+')');
+		throw new Error(err);
+	}		
+}
+
+const all = async(req,res) =>{
+	let db = req.db;
+	try{
+		let obj = await Patient.getPatients(db);
+		console.log('server-side: ' + obj.PATIENT_DATABASE.length+' patient(s) were returned');
+		res.send(obj);
+	}catch(err){
+		res.send('There was an error while retrieving all Patients. (err:'+err+')');
+		throw new Error(err);
+	}
+}
+
+module.exports = {
+	create,
+	getOne,
+	updateOne,
+	deleteOne,
+	all
+}
