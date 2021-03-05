ded17d7d88d5b2c1c5258d1c814552488e35bdbe
rtuck1998 <68603989+rtuck1998@users.noreply.github.com>
2021-03-05 17:12:57 -0330

    Add files via upload

--- router/router.js
+++ router/router.js
@@ -3,4 +3,8 @@ const router  = express.Router()
 
 const book_controller = require("../controllers/patients.js")
 
+router.get("/", book_controller.all)
+router.get("/:id", book_controller.getOne)
+router.get('/delete/:id', book_controller.deleteOne)
+
 module.exports = router
