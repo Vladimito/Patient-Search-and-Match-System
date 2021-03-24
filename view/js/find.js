32ebe8c6946ff172098ad59d32d4496c7afa8d17
rtuck1998 <68603989+rtuck1998@users.noreply.github.com>
2021-03-24 13:54:17 -0230

    Fixed several typos

--- view/js/find.js
+++ view/js/find.js
@@ -1,4 +1,4 @@
-(document).ready(function(){
+$(document).ready(function(){
     /**
      * This method will be reused everytime a new event happens in this tab.
      * If there's data to be showed, it will fill the form values,
@@ -21,19 +21,19 @@
             $("#find-suffix").val(data.suffix);
             $("#find-maiden").val(data.maiden);
             $("#find-marital").val(data.marital);
-            $("#race").val(data.race);
-            $("#ethnicity").val(data.ethnicity);
-            $("#gender").val(data.gender);
-            $("#birthplace").val(data.birthplace);
-            $("#address").val(data.address);
-            $("#city").val(data.city);
-            $("#state").val(data.state);
-            $("#county").val(data.county);
-            $("#zip").val(data.zip);
-            $("#lat").val(data.lat);
-            $("#lon").val(data.lon);
-            $("#healthExpenses").val(data.healthExpenses);
-            $("#healthCoverage").val(data.healthCoverage);
+            $("#find-race").val(data.race);
+            $("#find-ethnicity").val(data.ethnicity);
+            $("#find-gender").val(data.gender);
+            $("#find-birthplace").val(data.birthplace);
+            $("#find-address").val(data.address);
+            $("#find-city").val(data.city);
+            $("#find-state").val(data.state);
+            $("#find-county").val(data.county);
+            $("#find-zip").val(data.zip);
+            $("#find-lat").val(data.lat);
+            $("#find-lon").val(data.lon);
+            $("#find-healthExpense").val(data.healthExpenses);
+            $("#find-healthCoverage").val(data.healthCoverage);
         }else{
             $("#find-id").val("");
             $("#find-bday").val("");
@@ -47,19 +47,19 @@
             $("#find-suffix").val("");
             $("#find-maiden").val("");
             $("#find-marital").val("");
-            $("#race").val("");
-            $("#ethnicity").val("");
-            $("#gender").val("");
-            $("#birthplace").val("");
-            $("#address").val("");
-            $("#city").val("");
-            $("#state").val("");
-            $("#county").val("");
-            $("#zip").val("");
-            $("#lat").val("");
-            $("#lon").val("");
-            $("#healthExpenses").val("");
-            $("#healthCoverage").val("");
+            $("#find-race").val("");
+            $("#find-ethnicity").val("");
+            $("#find-gender").val("");
+            $("#find-birthplace").val("");
+            $("#find-address").val("");
+            $("#find-city").val("");
+            $("#find-state").val("");
+            $("#find-county").val("");
+            $("#find-zip").val("");
+            $("#find-lat").val("");
+            $("#find-lon").val("");
+            $("#find-healthExpense").val("");
+            $("#find-healthCoverage").val("");
         }      
     }
     /**
@@ -68,31 +68,31 @@
      */
      function assemble(){
         let c = {};
-        c.id = $("#add-id").val();
-        c.bday = $("add-bday").val();
-        c.dday = $("add-dday").val();
-        c.ssn = $("add-ssn").val();
-        c.drivers = $("add-drivers").val();
-        c.passport = $("add-passport").val();
-        c.prefix = $("add-prefix").val();
-        c.first = $("add-first").val();
-        c.last = $("add-last").val();
-        c.suffix = $("add-suffix").val();
-        c.maiden = $("add-maiden").val();
-        c.marital = $("add-marital").val();
-        c.race = $("add-race").val();
-        c.ethnicity = $("add-ethnicity").val();
-        c.gender = $("add-gender").val();
-        c.birthplace = $("add-birthplace").val();
-        c.address = $("add-address").val();
-        c.city = $("add-city").val();
-        c.state = $("add-state").val();
-        c.county = $("add-county").val();
-        c.zip = $("add-zip").val();
-        c.lat = $("add-lat").val();
-        c.lon = $("add-lon").val();
-        c.healthExpenses = $("add-healthExpenses").val();
-        c.healthCoverage = $("add-healthCoverage").val();
+        c.id = $("#find-id").val();
+        c.bday = $("#find-bday").val();
+        c.dday = $("#find-dday").val();
+        c.ssn = $("#find-ssn").val();
+        c.drivers = $("#find-drivers").val();
+        c.passport = $("#find-passport").val();
+        c.prefix = $("#find-prefix").val();
+        c.first = $("#find-first").val();
+        c.last = $("#find-last").val();
+        c.suffix = $("#find-suffix").val();
+        c.maiden = $("#find-maiden").val();
+        c.marital = $("#find-marital").val();
+        c.race = $("#find-race").val();
+        c.ethnicity = $("#find-ethnicity").val();
+        c.gender = $("#find-gender").val();
+        c.birthplace = $("#find-birthplace").val();
+        c.address = $("#find-address").val();
+        c.city = $("#find-city").val();
+        c.state = $("#find-state").val();
+        c.county = $("#find-county").val();
+        c.zip = $("#find-zip").val();
+        c.lat = $("#find-lat").val();
+        c.lon = $("#find-lon").val();
+        c.healthExpenses = $("#find-healthExpense").val();
+        c.healthCoverage = $("#find-healthCoverage").val();
         
         return c;
     }
@@ -125,7 +125,7 @@
         let patient_id = $("#find-id-search").val();
         let patient = assemble();
         $.ajax({
-            url: '/patients/'+patient_id,
+            url: '/system/update/'+patient_id,
             type: 'PUT',
             data: JSON.stringify(patient),
             contentType: 'application/json',                        
@@ -146,7 +146,7 @@
         event.preventDefault();
         let patient_id = $("#find-id-search").val();
         $.ajax({
-            url: '/patients/'+patient_id,
+            url: '/system/delete/'+patient_id,
             type: 'DELETE',
             contentType: 'application/json',                        
             success: function(response){
