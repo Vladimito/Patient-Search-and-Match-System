05ac091ceaf3947dd3fe3c2c8bb41d0b8a9b5016
Vladimito <vldandrd@gmail.com>
2021-03-25 14:26:30 -0230

    added new route for allSymps to work

--- router/router.js
+++ router/router.js
@@ -6,6 +6,7 @@ const patientController = require("../controllers/patients.js")
 router.post("/",patientController.create)
 router.get("/all", patientController.all)
 router.get("/:id", patientController.getOne)
+router.get("/:symptoms", patientController.allSymps)
 router.get("/delete/:id", patientController.deleteOne)
 router.put("/update/:id",patientController.updateOne)
 module.exports = router
