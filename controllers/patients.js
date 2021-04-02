315bb8dd25cf730a659fd4517f676f11abb7894a
Vladimito <vldandrd@gmail.com>
2021-04-02 12:40:30 -0230

    changed some of the routes to work better

--- controllers/patients.js
+++ controllers/patients.js
@@ -29,11 +29,12 @@ const updateOne = async(req, res) =>{
 	const patientID = req.params.id
 	let db = req.db;
 	try{
-		let msg = await Patient.update(db,patientID,pUpdate.bday,pUpdate.dday,pUpdate.ssn,pUpdate.passport,pUpdate.prefix,pUpdate.first,pUpdate.last,pUpdate.suffix,pUpdate.maiden,pUpdate.marital,pUpdate.race,pUpdate.ethnicity,pUpdate.gender,pUpdate.birthplace,pUpdate.address,pUpdate.city,pUpdate.county,pUpdate.zip,pUpdate.lat,pUpdate.lon,pUpdate.healthExpenses,pUpdate.healthCoverage);
+		console.log("pUpdate: " + JSON.stringify(pUpdate));
+		let msg = await Patient.update(db,patientID,pUpdate.id,pUpdate.bday,pUpdate.dday,pUpdate.ssn,pUpdate.passport,pUpdate.prefix,pUpdate.first,pUpdate.last,pUpdate.suffix,pUpdate.maiden,pUpdate.marital,pUpdate.race,pUpdate.ethnicity,pUpdate.gender,pUpdate.birthplace,pUpdate.address,pUpdate.city,pUpdate.county,pUpdate.zip,pUpdate.lat,pUpdate.lon,pUpdate.healthExpenses,pUpdate.healthCoverage);
 		res.send(msg);
 	}catch(err){
 		res.send('There was an error while updating that patient\'s information. (err:'+err+')');
-		throw new Error(err);
+		throw new Error(err.msg);
 	}
 }
 
@@ -44,8 +45,8 @@ const deleteOne = async (req, res) => {
 		let msg = await Patient.delete(db, patientID);
 		res.send(msg);	
 	}catch(err){
-		res.send('There was an error while deleting your Book. (err:'+err+')');
-		throw new Error(err);
+		res.send('There was an error while deleting your Book. (err:'+JSON.stringify(err.msg)+')');
+		throw new Error(JSON.stringify(err.msg));
 	}		
 }
 
